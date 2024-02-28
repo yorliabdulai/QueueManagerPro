71b73d10a5789c6354aaf6082a4798b9bf6f2730
yorliabdulai <yorliabdulai1@gmail.com>
2024-02-28 15:56:32 +0000

    first commit

---
+++ QueueManagerPro/Program.cs
@@ -0,0 +1,30 @@
+﻿git push -u origin mainusing System;
+using System.Collections.Generic;
+ class Program
+{
+    static void Main()
+    {
+        Queue<string> namesOfUsers = new Queue<string>();
+        namesOfUsers.Enqueue("Tawfiq");
+        namesOfUsers.Enqueue("Tiisu");
+        namesOfUsers.Enqueue("Abdulai");
+        namesOfUsers.Enqueue("Abdul Hafiz");
+
+        //Console.WriteLine($"Number of people in queue is: \n {namesOfUsers.Count}");
+
+        int index = 1;
+
+        foreach (string name in namesOfUsers)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"{index}. {name}");
+            index++;
+        }
+        int count = 1;
+        while(namesOfUsers.Count > 0)
+        {
+            Console.WriteLine($"{count}. {namesOfUsers.Dequeue()} Left the queue" );
+            count++;
+        }
+    }
+}
